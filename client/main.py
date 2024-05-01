a3610de12c18799723ced3f02dcef0e6e38ecbc3
Mateus Soares <mateus123soares@hotmail.com>
2024-05-01 03:25:48 -0300

    add

--- client/main.py
+++ client/main.py
@@ -1,4 +1,34 @@
 import requests
+import math
+
+def send_response_server(soma_pares,soma_impares,pi):
+    url = 'http://localhost:8080/response'
+    data = {
+        "soma_pares":soma_pares,
+        "soma_impares":soma_impares,
+        "pi":pi
+    }
+    response = requests.post(url, json=data)
+
+def calcular_soma_pares_impares(intervalo):
+    soma_pares = 0
+    soma_impares = 0
+    
+    for num in range(intervalo[0], intervalo[1]+1):
+        if num % 2 == 0:
+            soma_pares += num
+        else:
+            soma_impares += num
+            
+    return soma_pares, soma_impares
+
+def calcular_pi(intervalo):
+    soma_pi = 0
+    sinal = 1
+    for num in range(intervalo[0], intervalo[1]+1):
+        soma_pi += sinal * (1**num / ((2 * num) - 1))
+        sinal *= -1
+    return soma_pi
 
 def main():
     url = 'http://localhost:8080'
@@ -6,7 +36,15 @@ def main():
         'teste': '1'
     }
     response = requests.post(url, json=data)
-    print(response.text)
+    data = response.json()
+    print(data)
+    if data["range"] != "empty":
+        soma_pares, soma_impares = calcular_soma_pares_impares(data["range"])
+        pi = calcular_pi(data["range"])
+        send_response_server(soma_pares,soma_impares,pi)
+    else:
+        print("Finalizou")
+        exit(0)
 
 if __name__ == "__main__":
     main()
--- server/main.py
+++ server/main.py
@@ -4,15 +4,40 @@ import threading
 
 # Criando uma trava para sincronizar o acesso à lista divided_intervals
 lock = threading.Lock()
+soma_lock = threading.Lock()
+pi_soma=0
 
-def handle_post_request(client_socket, data, divided_intervals):
+def handle_post_response(json_data,conn):
+    global soma_lock
+    global pi_soma
+
+    print(json_data)
+    print(pi_soma)
+    pi_soma = pi_soma + json_data["pi"]
+    print("Calculo Parcial PI")
+    print(pi_soma)
+    print("Calculo Final")
+    print(4*pi_soma)
+
+
+    with soma_lock:  # Adquirindo a trava antes de acessar a lista
+        response_data = json.dumps({'response': 'ok'})
+        # Construir a resposta HTTP
+        response = f"HTTP/1.1 200 OK\nContent-Type: application/json\nContent-Length: {len(response_data)}\n\n{response_data}"
+
+        # Enviar a resposta ao cliente
+        conn.sendall(response.encode())
+        conn.close()
+
+
+def handle_post_request(client_socket, data, divided_intervals,order):
     global lock
 
     with lock:  # Adquirindo a trava antes de acessar a lista
         if len(divided_intervals) == 0:
             response_data = json.dumps({'range': 'empty'})
         else:
-            response_data = json.dumps({'range': divided_intervals.pop(0)})
+            response_data = json.dumps({'order':order,'range': divided_intervals.pop(0)})
 
     # Construir a resposta HTTP
     response = f"HTTP/1.1 200 OK\nContent-Type: application/json\nContent-Length: {len(response_data)}\n\n{response_data}"
@@ -21,7 +46,7 @@ def handle_post_request(client_socket, data, divided_intervals):
     client_socket.sendall(response.encode())
     client_socket.close()
 
-def handle_client(conn,divided_intervals):
+def handle_client(conn,divided_intervals,order):
     data = conn.recv(1024)
     if data:
         try:
@@ -30,7 +55,10 @@ def handle_client(conn,divided_intervals):
             body_index = lines.index("\r") + 1
             body = "\n".join(lines[body_index:])
             json_data = json.loads(body)
-            handle_post_request(conn, json_data, divided_intervals)
+            if '/response' in lines[0]:
+                handle_post_response(json_data,conn)
+            else:
+                handle_post_request(conn, json_data, divided_intervals,order)
         except Exception as e:
             print("Erro ao processar requisição:", e)
     conn.close()
@@ -38,21 +66,22 @@ def handle_client(conn,divided_intervals):
 def main():
     HOST = 'localhost'
     PORT = 8080
-    MAX_CONNECTIONS=12
-    PI_INTERVAL=15
+    MAX_CONNECTIONS=2
+    PI_INTERVAL=1000000
 
     divided_intervals = divide_interval(1, PI_INTERVAL, MAX_CONNECTIONS)
+    order=0
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
-
         print(f"Servidor escutando em {HOST}:{PORT}")
 
         while True:
             conn, addr = s.accept()
             print('Conectado por', addr)
-            thread = threading.Thread(target=handle_client, args=(conn,divided_intervals,))
+            order=order+1
+            thread = threading.Thread(target=handle_client, args=(conn,divided_intervals,order,))
             thread.start()
 
 def divide_interval(start, end, parts):
